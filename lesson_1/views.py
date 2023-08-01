09f56e39a34fdfd8ec61d4b4a136684f270ca821
isupovGitKraken <viktorisup@gmail.com>
2023-08-01 16:46:27 +0300

    lesson_3 completed

--- lesson_1/views.py
+++ lesson_1/views.py
@@ -3,12 +3,12 @@ from viktor_framework.templator import render
 
 class Index:
     def __call__(self, request):
-        return '200 OK', render('index.html', date=request.get('date', None))
+        return '200 OK', render('home.html', date=request.get('date', None))
 
 
 class About:
     def __call__(self, request):
-        return '200 OK', render('about.html', date=request.get('date', None))
+        return '200 OK', render('about1.html', date=request.get('date', None))
 
 
 class Contacts:
