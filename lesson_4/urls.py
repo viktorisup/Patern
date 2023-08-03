d805c3082a465da0ee61b97e778cd2a4df2c8f8a
Viktor-home <v.isupov@mail.ru>
2023-08-03 16:05:00 +0300

    lesson_4 completed

---
+++ lesson_4/urls.py
@@ -0,0 +1,27 @@
+from datetime import date
+from views import Index, About, StudyPrograms, Contacts, CoursesList, \
+    CreateCourse, CreateCategory, CategoryList, CopyCourse
+
+
+# front controller
+def secret_front(request):
+    request['date'] = date.today()
+
+
+def other_front(request):
+    request['key'] = 'key'
+
+
+fronts = [secret_front, other_front]
+
+routes = {
+    '/': Index(),
+    '/about/': About(),
+    '/contacts/': Contacts(),
+    '/courses-list/': CoursesList(),
+    '/create-course/': CreateCourse(),
+    '/create-category/': CreateCategory(),
+    '/category-list/': CategoryList(),
+    '/copy-course/': CopyCourse(),
+    '/study-programs/': StudyPrograms()
+}
---
+++ lesson_4/viktor_framework/templator.py
@@ -0,0 +1,12 @@
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
+
+
+def render(template_name, folder='templates', **kwargs):
+
+    env = Environment()
+    # указываем папку для поиска шаблонов
+    env.loader = FileSystemLoader(folder)
+    # находим шаблон в окружении
+    template = env.get_template(template_name)
+    return template.render(**kwargs)
