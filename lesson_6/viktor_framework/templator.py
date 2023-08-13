8927258328d01fbb222448ed585098f75211bbcb
Viktor-home <v.isupov@mail.ru>
2023-08-13 14:13:39 +0300

    lesson_6 completed

---
+++ lesson_6/viktor_framework/templator.py
@@ -0,0 +1,10 @@
+from jinja2 import Template, FileSystemLoader
+from jinja2.environment import Environment
+
+
+def render(template_name, folder='templates', **kwargs):
+
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    template = env.get_template(template_name)
+    return template.render(**kwargs)
